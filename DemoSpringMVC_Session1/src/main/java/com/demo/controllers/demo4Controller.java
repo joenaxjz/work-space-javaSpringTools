8406af9e909a3bb6fb0cdb75aa21a23ef9adffe8
jonashy <dta101203@gmail.com>
2023-09-30 14:42:40 +0700

    Spring Tools Java web
    30/09/2023

---
+++ DemoSpringMVC_Session1/src/main/java/com/demo/controllers/demo4Controller.java
@@ -0,0 +1,41 @@
+package com.demo.controllers;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.demo.helpers.FileHelper;
+import com.demo.services.CalculateService;
+import com.demo.services.CalculateServiceImpl;
+import com.demo.services.DemoService;
+import com.demo.services.DemoServiceImpl;
+import com.demo.services.ProductService;
+import com.demo.services.RectangleService;
+
+@Controller
+@RequestMapping({"demo4"}) //multi-link
+public class demo4Controller {
+	
+	
+	@RequestMapping(value = { "index", "" }, method = RequestMethod.GET)
+	public String index(ModelMap modelMap) {
+	
+		return "demo4/index";
+	}
+	
+	
+}
